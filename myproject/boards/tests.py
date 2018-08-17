2059e661ac8ef727a058fe1c7e0d6f29d88a96a9
Neil Duncan <neilduncan1234@gmail.com>
2018-08-17 17:56:19 +0100

    base layout

--- myproject/boards/tests.py
+++ myproject/boards/tests.py
@@ -11,11 +11,11 @@ class HomeTests(TestCase):
         self.response = self.client.get(url)
 
     def test_home_view_status_code(self):
-        self.assertEquals(self.response.status_code, 200)
+        self.assertEqual(self.response.status_code, 200)
 
     def test_home_url_resolves_home_view(self):
         view = resolve('/')
-        self.assertEquals(view.func, home)
+        self.assertEqual(view.func, home)
 
     def test_home_view_contains_link_to_topics_page(self):
         board_topics_url = reverse('board_topics', kwargs={'pk': self.board.pk})

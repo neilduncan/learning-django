027f2f6055e7c29aa7013d63e4268cbe50c48629
Neil Duncan <neilduncan1234@gmail.com>
2018-08-17 17:38:05 +0100

    fix more linting errors

--- myproject/boards/tests.py
+++ myproject/boards/tests.py
@@ -8,11 +8,11 @@ class HomeTests(TestCase):
     def test_home_view_status_code(self):
         url = reverse('home')
         response = self.client.get(url)
-        self.assertEquals(response.status_code, 200)
+        self.assertEqual(response.status_code, 200)
 
     def test_home_url_resolves_home_view(self):
         view = resolve('/')
-        self.assertEquals(view.func, home)
+        self.assertEqual(view.func, home)
 
 class BoardTopicsTests(TestCase):
     def setUp(self):
@@ -21,13 +21,13 @@ class BoardTopicsTests(TestCase):
     def test_board_topics_view_success_status_code(self):
         url = reverse('board_topics', kwargs={'pk': 1})
         response = self.client.get(url)
-        self.assertEquals(response.status_code, 200)
+        self.assertEqual(response.status_code, 200)
 
     def test_board_topics_view_not_found_status_code(self):
         url = reverse('board_topics', kwargs={'pk': 99})
         response = self.client.get(url)
-        self.assertEquals(response.status_code, 404)
+        self.assertEqual(response.status_code, 404)
 
     def test_board_topics_url_resolves_board_topics_view(self):
         view = resolve('/boards/1/')
-        self.assertEquals(view.func, board_topics)
+        self.assertEqual(view.func, board_topics)

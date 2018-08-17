c3d97a3e7ff4ebf5280e08ee52fd3e7c611bdbc0
Neil Duncan <neilduncan1234@gmail.com>
2018-08-17 17:33:10 +0100

    view and tests for boards

--- myproject/boards/views.py
+++ myproject/boards/views.py
@@ -1,6 +1,15 @@
 from django.shortcuts import render
+from django.http.response import Http404
+from django.core.exceptions import ObjectDoesNotExist
 from .models import Board
 
 def home(request):
     boards = Board.objects.all()
     return render(request, 'home.html', {'boards': boards})
+
+def board_topics(request, pk):
+    try:
+        board = Board.objects.get(pk=pk)
+    except ObjectDoesNotExist:
+        raise Http404
+    return render(request, 'topics.html', {'board': board})

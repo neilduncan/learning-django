4ca512dd61cd3a7d621b7445ff401f0e091eb7bc
Neil Duncan <neilduncan1234@gmail.com>
2018-08-23 16:55:20 +0100

    counts for topics and boards

--- boards/models.py
+++ boards/models.py
@@ -1,13 +1,20 @@
 from django.db import models
+from django.utils.text import Truncator
 from django.contrib.auth.models import User
 
 
 class Board(models.Model):
     name = models.CharField(max_length=30, unique=True)
     description = models.CharField(max_length=100)
-    
+
     def __str__(self):
         return self.name
+
+    def get_posts_count(self):
+        return Post.objects.filter(topic__board=self).count()
+
+    def get_last_post(self):
+        return Post.objects.filter(topic__board=self).order_by('-created_at').first()
 
 class Topic(models.Model):
     subject = models.CharField(max_length=255)
@@ -15,6 +22,8 @@ class Topic(models.Model):
     board = models.ForeignKey(Board, related_name='topics', on_delete=models.PROTECT)
     starter = models.ForeignKey(User, related_name='topics', on_delete=models.PROTECT)
 
+    def __str__(self):
+        return self.subject
 
 class Post(models.Model):
     message = models.TextField(max_length=4000)
@@ -23,3 +32,7 @@ class Post(models.Model):
     updated_at = models.DateTimeField(null=True)
     created_by = models.ForeignKey(User, related_name='posts', on_delete=models.PROTECT)
     updated_by = models.ForeignKey(User, null=True, related_name='+', on_delete=models.PROTECT)
+
+    def __str__(self):
+        truncated_message = Truncator(self.message)
+        return truncated_message.chars(30)
--- boards/views.py
+++ boards/views.py
@@ -1,4 +1,5 @@
 from django.contrib.auth.decorators import login_required
+from django.db.models import Count
 from django.shortcuts import render, redirect, get_object_or_404
 from .models import Board, Topic, Post
 from .forms import NewTopicForm, PostForm
@@ -9,7 +10,8 @@ def home(request):
 
 def board_topics(request, pk):
     board = get_object_or_404(Board, pk=pk)
-    return render(request, 'topics.html', {'board': board})
+    topics = board.topics.order_by('-last_updated').annotate(replies=Count('posts') - 1)
+    return render(request, 'topics.html', {'board': board, 'topics': topics})
 
 @login_required
 def new_topic(request, pk):
